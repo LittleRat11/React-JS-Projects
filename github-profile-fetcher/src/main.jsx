dd8cb3081843ab13189f8b6a8bacb89ae636b7e3
min khant <minkhant4287@gmail.com>
2025-01-10 00:33:18 +0630

    remove token

---
+++ github-profile-fetcher/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
+});
+
+    const client = new ApolloClient({
+        link: httpLink,
+        cache: new InMemoryCache(),
+})
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    
+  </StrictMode>,
+)
